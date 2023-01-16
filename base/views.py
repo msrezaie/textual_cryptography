3fcb84131cdc7cc0762077cccc3744b2973bd98c
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-01-16 13:17:43 -0500

    Added home template and routed URL

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from . models import Project, Profile
+ 
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    profile = Profile.objects.first()
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+    context = {'projects': projects, 'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
+    return render(request, 'base\home.html', context)
+
+def project_detail(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'base\project_detail.html', {'project': projectObj, 'tags': tags})
