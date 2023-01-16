aaab3d8547384abbb0d2f008eb0ff0304537c285
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-01-16 13:03:45 -0500

    created models
    profile, projects, skills, tag (for a particular stack) models added

---
+++ base/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=200, null= True, blank=True)
+    email = models.EmailField(max_length=200, null= True, blank=True)
+    long_bio = models.TextField(null= True, blank=True)
+    short_bio = models.CharField(max_length=200, null= True, blank=True)
+    location = models.CharField(max_length=200, null= True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, default="no-image.jpg")
+    social_github = models.CharField(max_length=200, null= True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null= True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.full_name)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null= True, blank=True)
+    name = models.CharField(max_length=200, null= True, blank=True)
+    description = models.TextField(null= True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(
+        null=True, blank=True, default="no-image.jpg")
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # unique attribute meaning: no other value can have
+    # the same value as the value of the following 'id'
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-rl&q=-hs3ew9uttrwb4v!n+9k1uf4p4a8(om!jny$so(achje8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
