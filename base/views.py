53218a68be54514a43b7c87cb3e971011f73cf42
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-01-24 17:09:19 -0500

    File names changed
    changed 'base.html' to 'index.html' and vice versa

--- base/views.py
+++ base/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from . models import Project, Profile
  
 
-def index(request):
+def base(request):
     profile = Profile.objects.first()
     projects = Project.objects.all()
     topSkills = profile.skill_set.exclude(description__exact="")
     otherSkills = profile.skill_set.filter(description="")
     context = {'profile': profile, 'projects': projects, 'topSkills': topSkills, 'otherSkills': otherSkills}
-    return render(request, "base/index.html", context)
+    return render(request, "base/base.html", context)
 
 def project_detail(request, pk):
     projectObj = Project.objects.get(id=pk)
