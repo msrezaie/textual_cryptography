b4b34b9b3ba1bec3640ff45607d4f07a7a160421
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-02-09 21:29:52 -0500

    background bug view on iOS fixed

--- cryptoden/views.py
+++ cryptoden/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from . models import Operation, Page
+from base.models import Profile
 
 # Create your views here.
 
 def index(request):
     operation = Operation.objects.all()
     page = Page.objects.first()
-    return render(request, "cryptoden/index.html", {'operations': operation, 'pages': page})
+    profile = Profile.objects.first()
+
+    context = {
+        'profile': profile,
+        'operations': operation,
+        'pages': page
+    }
+
+    return render(request, "cryptoden/index.html", context)
