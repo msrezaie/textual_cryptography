dc720c3541f4f9bd4a4b7c54fa13ae928fab357f
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-02-14 01:43:52 -0500

    new method for importing ciphers
    ciphers are now imported using importlib package

--- cryptoden/views.py
+++ cryptoden/views.py
@@ -2,19 +2,32 @@ from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from . models import Operation, Page
 from base.models import Profile
-
-from cryptoden.ciphers import caesar
+import json
+import importlib
 
 # Create your views here.
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        plainText = request.POST.get('plainText')
-        caesarKey = request.POST.get('caesarKey')
-        # process the plaintext and key with the caesar cipher
-        cipherText = caesar.encrypt(plainText, int(caesarKey))
-        # return the ciphertext as a response
-        return HttpResponse(cipherText)
+        recieved_data = json.loads(request.body.decode("utf-8"))
+        keys = recieved_data['keys']
+        for cipher_name, key_value in keys.items():
+        # Extract the cipher name from the key name
+            try:
+                # Import the cipher module
+                module = importlib.import_module(f'cryptoden.ciphers.{cipher_name}')
+                # Get the plaintext value
+                plaintext = recieved_data['text']
+                # Call the encrypt() or decrypt() method based on the operation
+                # result = module.encrypt(plaintext, int(key_value))
+                cipher_function = getattr(module, "encrypt")
+                # Use the result
+                # print()
+                result = cipher_function(plaintext, int(key_value))
+                return HttpResponse(result)
+
+            except ImportError:
+                print(f"Error: the cipher '{cipher_name}' was not found.")
 
     operation = Operation.objects.all()
     page = Page.objects.first()
