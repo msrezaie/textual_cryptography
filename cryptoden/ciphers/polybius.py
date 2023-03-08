d52786c8705e7b60e7fcfc8afbaa2dae64d25d1c
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-03-07 23:28:00 -0500

    Commented polybius cipher
    code for polybius cipher is refactored for better readability and also commented

--- cryptoden/ciphers/polybius.py
+++ cryptoden/ciphers/polybius.py
@@ -1,3 +1,4 @@
+# Create a dictionary to map characters to cipher values
 field = {
     '0': ' ', '11': 'A', '12': 'B', '13': 'C', '14': 'D', '15': 'E', '21': 'F', '22': 'G', '23': 'H', '24': 'I' or 'J',
     '25': 'K', '31': 'L',
@@ -6,34 +7,36 @@ field = {
     '54': 'Y', '55': 'Z'
 }
 
-values = []
-keys = []
-for k, v in field.items():
-    values.append(v)
-    keys.append(k)
+# Extract values and keys from the dictionary
+values = [v for v in field.values()]
+keys = [k for k in field.keys()]
 
 def encrypt(text, key='null'):
     try:
         text = text.upper()
+        # Create an empty list to store the cipher values
         keys = []
+        # Iterate over each character in the input text
         for i in text:
+            # If the character is not in the field values, append it as is to the list
             if i not in values:
                 keys.append(i)
             else:
+                # If the character is a space, append 0 as the cipher value
                 if i == " ":
-                    row = 0
-                    cipher = row
+                    cipher = '0'
                 else:
-                    row = int((ord(i) - ord('A')) / 5) + 1
-                    col = int((ord(i) - ord('A')) % 5) + 1
-                    cipher = str(row) + str(col)
+                    # Calculate the row and column values based on the character's position in the alphabet
+                    cipher = str((ord(i) - ord('A')) // 5 + 1) + str((ord(i) - ord('A')) % 5 + 1)
+                    # If the cipher value is greater than or equal to 25, subtract 1 from it
                     if int(cipher) >= 25:
-                        cipher = int(cipher) - 1
-                        if int(cipher) == 30 or int(cipher) == 40 or int(cipher) == 50 or int(cipher) == 60:
-                            cipher = int(cipher) - 5
+                        cipher = str(int(cipher) - 1)
+                        # If the new cipher value is 30, 40, 50 or 60, subtract 5 from it
+                        if cipher in ['30', '40', '50', '60']:
+                            cipher = str(int(cipher) - 5)
 
                 keys.append(int(cipher))
-
+        # Join the cipher values into a string and return it
         return ' '.join(str(x) for x in keys)
     except ValueError:
         return "Error!"
@@ -42,13 +45,11 @@ def encrypt(text, key='null'):
 def decrypt(cipher, key='null'):
     cipher = cipher.split()
     try:
-        res = []
-        for i in cipher:
-            if i in keys:
-                res.append(field[i])
-            else:
-                res.append(i)
-
-        return ''.join(res)
+        # Create a dictionary to map cipher values back to characters
+        cipher_map = {k: v for k, v in field.items()}
+        # Iterate over each cipher value in the list and map it back to a character using the cipher_map dictionary
+        result = [cipher_map.get(i, i) for i in cipher]
+        # Join the characters into a string and return it
+        return ''.join(result)
     except ValueError:
         return "Error!"
