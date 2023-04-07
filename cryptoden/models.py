6ba0d5f59ba77bf75f7b5051342a2a8d6cd19369
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-04-06 23:34:51 -0400

    table for cryptoden users
    integrated django built-in Users model with a table for cryptoden users

--- cryptoden/models.py
+++ cryptoden/models.py
@@ -1,6 +1,16 @@
 import os
+from django.contrib.auth.models import User
 from django.db import models
 
+class CryptodenUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.user
+    
 class Page(models.Model):
     background = models.ImageField(upload_to='images/', null=True, blank=True)
     
