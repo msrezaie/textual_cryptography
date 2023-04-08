d544184df1102613f9579bba509df639d462a39f
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-04-07 23:26:42 -0400

    integrated signals
    for user authentication, signals are used to automatically create an entry to CryptodenUser model whenever a user is registered

--- cryptoden/apps.py
+++ cryptoden/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CryptodenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cryptoden'
+
+    def ready(self):
+        import cryptoden.signals
---
+++ cryptoden/signals.py
@@ -0,0 +1,29 @@
+from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.models import User
+from . models import CryptodenUser
+
+# instance represents the instance/object of the sender, in this case the sender would be the object of the CryptodenUser model
+# Define a signal handler function for creating a CryptodenUser instance when a new User is created
+def createCryptodenUser(sender, instance, created, **kwargs):
+    # Check if a new User instance was just created
+    if created:
+        # Create a new CryptodenUser instance with the same attributes as the User instance
+        cryptodenUser = CryptodenUser.objects.create(
+            user=instance,
+            username=instance.username,
+            email=instance.email,
+            name=instance.first_name
+        )
+
+# Define a signal handler function for deleting the associated User instance when a CryptodenUser instance is deleted
+def deleteCryptodenUser(sender, instance, **kwargs):
+    # Get the User instance associated with the deleted CryptodenUser instance
+    user = instance.user
+    # Delete the User instance
+    user.delete()
+
+# Connect the createCryptodenUser function to the post_save signal of the User model
+post_save.connect(createCryptodenUser, sender=User)
+
+# Connect the deleteCryptodenUser function to the post_delete signal of the CryptodenUser model
+post_delete.connect(deleteCryptodenUser, sender=CryptodenUser)
