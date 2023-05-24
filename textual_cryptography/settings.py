3123596c3a7779ad27bf91f43e567c3dca5b31ec
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-05-24 00:18:49 -0400

    renamed project level directory

--- textual_cryptography/settings.py
+++ textual_cryptography/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cryptoden.pythonanywhere.com', '127.0.0.1']
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    },  
 ]
 
 
